5a1d902b7df51fc541afd5331f638fab07d83b89
prasann <prasann.vyawahare81@gmail.com>
2022-06-29 12:39:34 +0530

    android code

--- android/app/src/main/kotlin/com/example/solo_flutter_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/solo_flutter_example/MainActivity.kt
@@ -1,6 +1,58 @@
 package com.example.solo_flutter_example
 
+import android.util.Log
+import androidx.annotation.NonNull
+import com.example.solo_flutter_example.network.api.API
+import com.example.solo_flutter_example.network.api.RetrofitHelper
+import com.example.solo_flutter_example.network.model.Model
+import com.google.gson.JsonElement
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity: FlutterActivity() {
+
+    private val CHANNEL = "com.example.solo_flutter_example/getUserDetails"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+                call, result ->
+            // This method is invoked on the main thread.
+            // TODO
+    val arguments= call.arguments as Map<String,String>
+            val token=arguments["access_token"]
+
+
+            val quotesApi = RetrofitHelper.getInstance().create(API::class.java)
+            // launching a new coroutine
+            GlobalScope.launch(Dispatchers.Main) {
+
+               val resu = token?.let { getHeaderMap(it) }?.let { quotesApi.getQuotes(it) }
+                if (resu!= null) {
+
+                    val model: Model? = resu?.body()
+                  //  val name=model?.first_name.toString()
+                    Log.d("logmeee: ", model?.first_name.toString())
+
+                    val map = mapOf("first_name" to "${ model?.first_name.toString()}")
+
+                   // result.success(model?.first_name.toString())
+                    result.success(map)
+
+                }
+            }
+        }
+    }
+    private fun getHeaderMap(tokan: String): Map<String, String> {
+        val headerMap = mutableMapOf<String, String>()
+        headerMap["Content-Type"] = "application/json"
+        headerMap["accept"] = "application/json"
+        headerMap["Authorization"] = "Bearer $tokan"
+        return headerMap
+    }
 }
---
+++ android/app/src/main/kotlin/com/example/solo_flutter_example/network/model/Address.kt
@@ -0,0 +1,12 @@
+package com.example.solo_flutter_example.network.model
+
+data class Address(
+    val address1: String,
+    val address2: Any,
+    val addressId: Int,
+    val city: String,
+    val country: String,
+    val display_preference: String,
+    val state: String,
+    val zip: String
+)
